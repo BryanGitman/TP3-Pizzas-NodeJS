bfeaad517aabef3a79081ce241b8f9981cbc2cf5
MATIAS LEONEL ZALCMAN <47028030@ort>
2023-05-05 10:03:58 -0300

    frontend terminado

---
+++ API/src/models/pizza.js
@@ -0,0 +1,10 @@
+export class Pizza
+{
+    constructor(nombre, libreGluten, importe, descripcion)
+    {
+        this.nombre = nombre;
+        this.libreGluten = libreGluten;
+        this.importe = importe;
+        this.descripcion = descripcion;
+    }
+}
